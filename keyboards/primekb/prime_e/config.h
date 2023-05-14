39b513e0e3b24c1ee7aa19a69200559471e461e0
Jason Zheng <jasonzheng@Jasons-Mac-mini.attlocal.net>
2023-05-14 04:03:32 -0700

    Add keymap and conigs for prime_e

--- keyboards/primekb/prime_e/config.h
+++ keyboards/primekb/prime_e/config.h
@@ -31,3 +31,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LOCKING_RESYNC_ENABLE
 
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+/* Mouse key configs */
+#define MK_COMBINED
+
+#define MOUSEKEY_DELAY 0
+
+#define MOUSEKEY_INTERVAL 16
+
+#define MOUSEKEY_MOVE_DELTA 6
+
+#define MOUSEKEY_MAX_SPEED 8
+
+#define MOUSEKEY_TIME_TO_MAX 16
--- keyboards/primekb/prime_e/rules.mk
+++ keyboards/primekb/prime_e/rules.mk
@@ -2,11 +2,12 @@
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = no       # Mouse keys
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 AUDIO_ENABLE = no           # Audio output
+VIA_ENABLE = yes
 
 DEFAULT_FOLDER = primekb/prime_e/std
