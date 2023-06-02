77ab0ac7493aeda4866bc92d4ce0f62ccd15b1fd
Jason Zheng <jasonzheng@Jasons-Mac-mini.local>
2023-06-02 00:55:47 -0700

    Comment out rgb code; add extra layer

--- keyboards/crkbd/r2g/rules.mk
+++ keyboards/crkbd/r2g/rules.mk
@@ -1,6 +1,3 @@
-OLED_ENABLE     = yes
-OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
-RGBLIGHT_ENABLE = yes
-
+VIA_ENABLE = yes
 SPLIT_KEYBOARD = yes
--- keyboards/crkbd/rev1/rules.mk
+++ keyboards/crkbd/rev1/rules.mk
@@ -1,1 +1,2 @@
 SPLIT_KEYBOARD = yes
+BOOTLOADER = atmel-dfu
